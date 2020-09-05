4b1a1b3e5f2922d40768f4c1fdad42ddba0d50a6
dstubbs <dstubbs@solutionlabs.co.uk>
2020-09-05 18:09:02 +0100

    Added credentials

---
+++ credentials/credentials.go
@@ -0,0 +1,62 @@
+package credentials
+
+/*
+Get the client credentials for Secret Manager.
+*/
+
+import (
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	"context"
+	"encoding/json"
+	"fmt"
+	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
+	"log"
+	"os"
+)
+
+// --------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------
+type Credentials struct {
+	PubSubTopic string     `json:"pubsub_topic"`
+	Clientid    int        `json:"clientid"`
+	Siteid      int        `json:"locationid"`
+	Providers   []Provider `json:"providers"`
+}
+
+// --------------------------------------------------------------------------------------------------------------
+type Provider struct {
+	Provider string `json:"provider"`
+	Code     string `json:"cashless_code"`
+	Url      string `json:"url"`
+	Uname    string `json:"username"`
+	Password string `json:"password"`
+	APIKey   string `json:"apikey"`
+}
+
+// --------------------------------------------------------------------------------------------------------------
+
+func GetCredentials(clientid string, version int) (Credentials, error) {
+
+	ctx := context.Background()
+	client, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		log.Println(fmt.Errorf("failed to create secretmanager client: %v", err))
+	}
+
+	req := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: fmt.Sprintf("projects/%s/secrets/%s/versions/%d", os.Getenv("PROJECTID"), clientid, version),
+	}
+
+	result, err := client.AccessSecretVersion(ctx, req)
+	if err != nil {
+		log.Println(fmt.Errorf("failed to access secret version: %v", err))
+	}
+
+	dbc := Credentals{}
+
+	err = json.Unmarshal(result.Payload.Data, &dbc)
+	return dbc, err
+
+}
+
+// --------------------------------------------------------------------------------------------------------------
