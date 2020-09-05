eefe5cfdb073bcac1db3d5f4e3c87b63a53c8c95
dstubbs <dstubbs@solutionlabs.co.uk>
2020-09-05 17:07:24 +0100

    First shell for payment data.

---
+++ payments/payments.go
@@ -0,0 +1,20 @@
+package payments
+
+import "time"
+
+type Data struct {
+	Clientid  int       `json:"clientid"`
+	Siteid    int       `json:"siteid"`
+	Provider  string    `json:"provider"`
+	MachineId string    `json:"machine_id"`
+	VRM       string    `json:"vrm"`
+	DateFrom  string    `json:"date_from"`
+	DateTo    string    `json:"date_to"`
+	PaymentID string    `json:"payment_id"`
+	RawData   string    `json:"raw_data"`
+	Received  time.Time `json:"received"`
+}
+
+func (d *Data) Save() {
+
+}
