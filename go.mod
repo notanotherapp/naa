f3298cb20d2c96addd906dbc5bf23c53f2146a8c
dstubbs <dstubbs@solutionlabs.co.uk>
2020-09-05 17:33:27 +0100

    Pubsub send added

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/notanotherapp/naa
 
 go 1.14
+
+require cloud.google.com/go/pubsub v1.6.1
--- naa/payments/payments.go
+++ naa/payments/payments.go
@@ -1,9 +1,14 @@
 package payments
 
-import "time"
-
-const PAYBYPHONE = "paybyphone"
-const TAP2PARK = "tap2park"
+import (
+	"cloud.google.com/go/pubsub"
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+	"strconv"
+	"time"
+)
 
 type Data struct {
 	Clientid  int       `json:"clientid"`
@@ -18,6 +23,54 @@ type Data struct {
 	Received  time.Time `json:"received"`
 }
 
-func (d *Data) Save() {
+func (d *Data) Save(pstopic string) error {
+
+	if d.Siteid == 0 {
+		return errors.New("missing siteid")
+	}
+
+	if d.Clientid == 0 {
+		return errors.New("missing siteid")
+	}
+
+	if len(d.VRM) == 0 {
+		return errors.New("missing vehicle information")
+	}
+
+	ctx := context.Background()
+
+	client, err := pubsub.NewClient(ctx, "notanotherapp")
+
+	defer client.Close()
+
+	if err != nil {
+		return err
+	}
+
+	topic := client.Topic(pstopic)
+
+	attrib := make(map[string]string)
+
+	attrib["siteid"] = strconv.Itoa(d.Siteid)
+
+	payload, err := json.Marshal(&d)
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	msg := &pubsub.Message{
+		Data: payload,
+		//Attributes: attrib,
+	}
+
+	if _, err := topic.Publish(ctx, msg).Get(ctx); err != nil {
+		return err
+	}
+
+	topic.Stop()
+
+	return nil
 
 }
